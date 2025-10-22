99f8e76d0b0632ae387227163a267367a7214957
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-22 00:09:20 -0400

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 
 import MyRoutes from "./routers/routes";
+import { useState } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
+import { Ligth, Dark } from "./theme";
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const themeStyle = theme === "light" ? Ligth : Dark;
 
   return (
     <>
-      <MyRoutes />
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <ThemeProvider theme={themeStyle}>
+        <MyRoutes />
+      </ThemeProvider>
+    </ThemeContext.Provider>
     </>
   )
 }
--- src/components/molecula/Button.jsx
+++ src/components/molecula/Button.jsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 
-export default function Button() {
-    return (
-        <ContainerBtn>
-            <button>Enter</button>
-        </ContainerBtn>
-    );
+export default function Button({ funcion, titulo, bgcolor, icono }) {
+  return (
+    <ContainerBtn type="submit">
+        <span className="icono">{icono}</span>
+      <span className="btn" onClick={funcion}>
+        {titulo}
+      </span>
+    </ContainerBtn>
+  );
 }
 
 const ContainerBtn = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-self: center;
-    text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  border: none;
+  .icono {
+    margin-right: 10px;
+  }
 `;

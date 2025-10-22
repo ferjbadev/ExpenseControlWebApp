e5b4f3fc82421498f81ed0ecfcc3e33606c826be
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-22 01:21:01 -0400

    Update LoginTemplate.jsx

--- src/components/templates/LoginTemplate.jsx
+++ src/components/templates/LoginTemplate.jsx
@@ -4,30 +4,50 @@ import {v as V} from "../../styles/variables";
 
 export default function LoginTemplate() {
     return (
-        <Container>
-            <div>
-                <span> version 1.0.0</span>
+        <Container imgFondo={V.imagenfondo}>
+            <div className="contentCard">
+                <span className="version"> version 1.0.0</span>
                 <div>
                     <img src="" alt="" />
                 </div>
                 <Titulo>Expense Control</Titulo>
                 <p style={{ fontSize: "1.5rem" }}>Take control of your expenses</p>
-            </div>
 
             {/* Boton */}
             <ContainerBtn>
                 <Button titulo="Login with Google"  icono={<V.iconogoogle/>} ></Button>
             </ContainerBtn>
+            </div>
         </Container>
     );  
 }
 
 const Container = styled.div`
+    background-image: url(${props => props.imgFondo});
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
+    color: rgba(255, 255, 255, 0.87);
+    text-align: center;
+
+    .contentCard {
+        background-color: #131313;
+        border-radius: 20px;
+        gap: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 20px;
+        box-shadow: 8px 5px 18px 3px rgba(5, 152, 34, 0.35);
+        .version {
+            color: #727272;
+            text-align: start;
+        }
+    }
+
 `;
 
 const Titulo = styled.span`

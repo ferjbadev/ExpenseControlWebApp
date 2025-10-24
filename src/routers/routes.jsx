da83de3e026a51b1646b9dc8f7e9d1a5fab79f57
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 10:03:59 -0400

    Upgrade routers: add protectedRoutes

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,14 +1,20 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "../pages/login";
 import Home from "../pages/Home";
+import { ProtectedRoute } from "../hooks/ProtectedRoute";
+import { UserAuth } from "../contexts/AuthContext";
 
 
 export default function MyRoutes() {
+    const {user} = UserAuth();
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/asdad" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route element={<ProtectedRoute user={user} redirectTo="/login" />}>
+                    <Route path="/home" element={<Home />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );

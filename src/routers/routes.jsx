6a25c7c0bfce8984af245c2830485b2bb9387108
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 14:29:08 -0400

    Update routes.jsx

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import { ProtectedRoute } from "../hooks/ProtectedRoute";
@@ -9,14 +9,12 @@ export default function MyRoutes() {
     const {user} = UserAuth();
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route element={<ProtectedRoute user={user} redirectTo="/login" />}>
-                    <Route path="/home" element={<Home />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoute user={user} redirectTo="/login" />}>
+                <Route path="/home" element={<Home />} />
+            </Route>
+        </Routes>
     );
-}   
+}

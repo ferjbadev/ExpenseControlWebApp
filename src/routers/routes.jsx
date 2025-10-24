68b31f50eca612b02519be37b74a1107c6062e2d
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 13:03:31 -0400

    Update routes.jsx

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Login from "../pages/login";
+import Login from "../pages/Login";
 import Home from "../pages/Home";
 import { ProtectedRoute } from "../hooks/ProtectedRoute";
 import { UserAuth } from "../contexts/AuthContext";

8d8bef6e44b1ca5ffc03730eaf6efed0aba2746c
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 12:29:49 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@
 import MyRoutes from "./routers/routes";
 import { useState } from "react";
 import { ThemeContext } from "./contexts/ThemeContext";
-import { Light, Dark, AuthContextProvider } from "./styles/theme";
+import { Light, Dark } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -12,14 +16,16 @@ function App() {
 
   return (
     <>
-    <ThemeContext.Provider value={{theme, setTheme}}>
-      <ThemeProvider theme={themeStyle}>
-      <AuthContextProvider>
-        <MyRoutes />
-        <ReactQueryDevtools initialIsOpen={true}/>
-      </AuthContextProvider>
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContext.Provider value={{theme, setTheme}}>
+        <ThemeProvider theme={themeStyle}>
+        <AuthContextProvider>
+          <MyRoutes />
+          <ReactQueryDevtools initialIsOpen={true}/>
+        </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </QueryClientProvider>
     </>
   )
 }

4c174a136475adf6753f98b4b67f4b61b6910087
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 09:03:56 -0400

    Update AuthStore.jsx

--- src/store/AuthStore.jsx
+++ src/store/AuthStore.jsx
@@ -8,12 +8,29 @@ export const useAuthStore = create((set) => ({
             const { data, error } = await supabase.auth.signInWithOAuth({
                 provider: "google",
             });
+            if (error) {
+                throw new Error("Failed to sign in with Google. Please try again.");
+               
+            }
+            set({ isAuth: true });
+            return data;
         } catch (error) {
             console.log(error);
         }
     },
-    user: null,
-    setUser: (user) => set({ user }),
+
+    signOut: async () => {
+        try {
+            const { error } = await supabase.auth.signOut();
+            set({ isAuth: false });
+            if (error) {
+                throw new Error("Failed to sign out. Please try again.");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    
 }))
 
 

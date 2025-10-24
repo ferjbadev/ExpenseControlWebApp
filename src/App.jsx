4a3ffa20d3bcd58bff6c775673de4a92d5473aa9
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-24 14:28:55 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import { useState } from "react";
 import { ThemeContext } from "./contexts/ThemeContext";
 import { Light, Dark } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthContextProvider } from "./contexts/AuthContext";
-
-const queryClient = new QueryClient();
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -16,7 +13,6 @@ function App() {
 
   return (
     <>
-    <QueryClientProvider client={queryClient}>
       <ThemeContext.Provider value={{theme, setTheme}}>
         <ThemeProvider theme={themeStyle}>
         <AuthContextProvider>
@@ -25,7 +21,6 @@ function App() {
         </AuthContextProvider>
         </ThemeProvider>
       </ThemeContext.Provider>
-    </QueryClientProvider>
     </>
   )
 }

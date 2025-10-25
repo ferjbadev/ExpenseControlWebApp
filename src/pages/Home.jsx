a68f0d3620eaa7e924fea1ce05bfcdd407d2402c
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-25 19:14:13 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -136,6 +136,8 @@ export default function Home() {
                     onClose={handleCloseModal}
                 />
             </Modal>
+
+            <Footer />
         </Container>
     );
 }
